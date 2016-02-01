14d9cf9b1dcb9eab3703331c36e4a94b1107bd06
Ryan Rempel <rgrempel@gmail.com>
2016-01-31 23:29:52 -0600

    Add `Signal` to set of `Elm.Default` imports.

--- src/Elm/Default.purs
+++ src/Elm/Default.purs
@@ -13,6 +13,6 @@ import Elm.Debug as Virtual
 import Elm.List (List(), (:)) as Virtual
 import Data.Maybe (Maybe (Just, Nothing)) as Virtual
 import Elm.Result (Result (Ok, Err)) as Virtual
--- import Signal (Signal ()) as Virtual
+import Elm.Signal (Signal ()) as Virtual
 
 
