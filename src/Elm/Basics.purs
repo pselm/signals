8a88d434ce4accca63d9c35d77a98a4bb003807d
Ryan Rempel <rgrempel@gmail.com>
2016-02-05 20:17:26 -0600

    Update some documentation.

--- src/Elm/Basics.purs
+++ src/Elm/Basics.purs
@@ -1,3 +1,14 @@
+
+-- | The Elm equivalent of Purescript's `Prelude`.
+-- |
+-- | Note that many of these functions are re-exported from other modules in
+-- | Purescript. The goal is to bundle together the same things that Elm's `Basics` module
+-- | bundles together.
+-- | 
+-- | In Elm, these functions are all imported by default. Purescript does not
+-- | have default imports, so you will need to import this manually. Alternatively,
+-- | you can import `Elm.Default`, which will import all of Elm's default imports.
+
 module Elm.Basics 
     ( module Virtual
     , Order(..)
@@ -44,37 +55,40 @@ import Math (cos, sqrt, log, pi, sin, atan2)
 import Data.Int (round, toNumber)
 
 
+-- | The Purescript equivalent of Elm's `Float` is `Number`.
 type Float = Number
+
+
+-- | The Purescript equivalent of Elm's `Bool` is `Boolean`.
 type Bool = Boolean
 
 
-{-| Convert radians to standard Elm angles (radians). -}
+-- | Convert radians to standard Elm angles (radians).
 radians :: Float -> Float
 radians = id
 
 
-{-| Convert degrees to standard Elm angles (radians). -}
+-- | Convert degrees to standard Elm angles (radians).
 degrees :: Float -> Float
 degrees d =
     d * pi / 180.0
 
 
-{-| Convert turns to standard Elm angles (radians).
-One turn is equal to 360&deg;.
--}
+-- | Convert turns to standard Elm angles (radians).
+-- | One turn is equal to 360&deg;.
 turns :: Float -> Float
 turns t =
     2.0 * pi * t
 
 
-{-| Convert polar coordinates {r, theta} to Cartesian coordinates {x, y}.
-
-Note that the Elm version uses tuples ... it seemed like this was a good
-candidate for records instead.
-
-If you want some more sophisticated handling of complex numbers, see
-[purescript-complex](http://pursuit.purescript.org/packages/purescript-complex).
--}
+-- | Convert polar coordinates `{r, theta}` to Cartesian coordinates `{x, y}`.
+-- |
+-- | Note that the Elm version uses tuples ... it seemed like this was a good
+-- \ candidate for records instead. (Since Purescript does not have tuple
+-- | literals, Elm code using Tuples nees some modification in any event).
+-- |
+-- | If you want some more sophisticated handling of complex numbers, see
+-- | [purescript-complex](http://pursuit.purescript.org/packages/purescript-complex).
 fromPolar :: {r :: Float, theta :: Float} -> {x :: Float, y :: Float}
 fromPolar {r, theta} =
     { x: r * cos theta
@@ -82,14 +96,14 @@ fromPolar {r, theta} =
     }
 
 
-{-| Convert Cartesian coordinates {x, y} to polar coordinates {r, theta}.
-
-Note that the Elm version uses tuples ... it seemed like this was a good
-candidate for records instead.
-
-If you want some more sophisticated handling of complex numbers, see
-[purescript-complex](http://pursuit.purescript.org/packages/purescript-complex).
--}
+-- | Convert Cartesian coordinates `{x, y}` to polar coordinates `{r, theta}`.
+-- |
+-- | Note that the Elm version uses tuples ... it seemed like this was a good
+-- \ candidate for records instead. (Since Purescript does not have tuple
+-- | literals, Elm code using Tuples nees some modification in any event).
+-- |
+-- | If you want some more sophisticated handling of complex numbers, see
+-- | [purescript-complex](http://pursuit.purescript.org/packages/purescript-complex).
 toPolar :: {x :: Float, y :: Float} -> {r :: Float, theta :: Float}
 toPolar {x, y} =
     { r: sqrt (x * x + y * y)
@@ -99,7 +113,9 @@ toPolar {x, y} =
 
 infixl 7 //
 
-{-| Integer division. The remainder is discarded. -}
+-- | Integer division. The remainder is discarded.
+-- |
+-- | In Purescript, you can simply use `/`.
 (//) :: forall a. (ModuloSemiring a) => a -> a -> a
 (//) = Prelude.(/)
 
@@ -108,26 +124,25 @@ infixl 7 //
 --
 -- infixl 7 `rem`
 
-{-| Find the remainder after dividing one number by another.
-
-     7 `rem` 2 == 1
-    -1 `rem` 4 == -1
-
-Note that this is the Purescript `Prelude.mod`.
--}
+-- | Find the remainder after dividing one number by another.
+-- |
+-- |     7 `rem` 2 == 1
+-- |     -1 `rem` 4 == -1
+-- |
+-- | Equivalent to Purescript's `Prelude.mod`.
 rem :: forall a. (ModuloSemiring a) => a -> a -> a
 rem = Prelude.mod
 
 
 infixl 7 %
 
-{-| Perform [modular arithmetic](http://en.wikipedia.org/wiki/Modular_arithmetic).
-
-     7 % 2 == 1
-    -1 % 4 == 3
-
-Note that this represents different semantics than the Purescript `Prelude.mod`.
--}
+-- | Perform [modular arithmetic](http://en.wikipedia.org/wiki/Modular_arithmetic).
+-- |
+-- |      7 % 2 == 1
+-- |     -1 % 4 == 3
+-- |
+-- | Note that this is not the same as Purescript's `Prelude.mod` --
+-- | for that, see `Basics.rem`.
 (%) :: forall a. (Semiring a, Ring a, Ord a, ModuloSemiring a) => a -> a -> a
 (%) a b =
     let
@@ -158,6 +173,7 @@ Note that this represents different semantics than the Purescript `Prelude.mod`.
             else m
 
 
+-- | A class for things that can be raised to a power.
 class Pow a where
     pow :: a -> a -> a
 
@@ -170,15 +186,14 @@ instance floatFloatPow :: Pow Number where
 
 infixr 8 ^
 
-{-| Exponentiation
-
-    3^2 == 9
--}
+-- | Exponentiation
+-- |
+-- |     3^2 == 9
 (^) :: forall a. (Pow a) => a -> a -> a 
 (^) = pow
 
 
-{-| Take the absolute value of a number. -}
+-- | Take the absolute value of a number.
 abs :: forall a. (Ring a, Ord a) => a -> a
 abs a =
     if a >= zero
@@ -186,66 +201,70 @@ abs a =
         else negate a
 
 
-{-| Calculate the logarithm of a number with a given base.
-
-    logBase 10 100 == 2
-    logBase 2 256 == 8
--}
+-- | Calculate the logarithm of a number with a given base.
+-- |
+-- |     logBase 10 100 == 2
+-- |     logBase 2 256 == 8
 logBase :: Float -> Float -> Float
 logBase base n =
     log n / log base
 
 
-{-| Represents the relative ordering of two things.
-The relations are less than, equal to, and greater than.
--}
+-- | Represents the relative ordering of two things.
+-- | The relations are less than, equal to, and greater than.
+-- |
+-- | Equivalent to Purescript's `Ordering`.
 type Order = Ordering
 
 
-{-| The exclusive-or operator. `True` if exactly one input is `True`. -}
+-- | The exclusive-or operator. `True` if exactly one input is `True`.
 xor :: forall a. (Prelude.BooleanAlgebra a) => a -> a -> a
 xor a b =
     (a && not b) || (not a && b)
 
 
-{-| Truncate a number, rounding towards zero. -}
+-- | Truncate a number, rounding towards zero.
 foreign import truncate :: Float -> Int
 
 
-{-| Ceiling function, rounding up. -}
+-- | Ceiling function, rounding up.
+-- |
+-- | Equivalent to Purescript's `ceil`.
 ceiling :: Float -> Int
 ceiling = Data.Int.ceil
 
 
-{-| Convert an integer into a float. -}
+-- | Convert an integer into a float.
+-- |
+-- | Equivalent to Purescript's `toNumber`.
 toFloat :: Int -> Float
 toFloat = Data.Int.toNumber
 
 
-{-| Determine whether a float is positive or negative infinity.
-
-    isInfinite (0/0)     == False
-    isInfinite (sqrt -1) == False
-    isInfinite (1/0)     == True
-    isInfinite 1         == False
-
-Notice that NaN is not infinite! For float `n` to be finite implies that
-`not (isInfinite n || isNaN n)` evaluates to `True`.
-
-Note that this is not equivalent to the negation of Javascript's `isFinite()`.
--}
+-- | Determine whether a float is positive or negative infinity.
+-- |
+-- |     isInfinite (0/0)     == False
+-- |     isInfinite (sqrt -1) == False
+-- |     isInfinite (1/0)     == True
+-- |     isInfinite 1         == False
+-- | 
+-- | Notice that NaN is not infinite! For float `n` to be finite implies that
+-- | `not (isInfinite n || isNaN n)` evaluates to `True`.
+-- | 
+-- | Note that this is not equivalent to the negation of Javascript's `isFinite()`.
 isInfinite :: Float -> Boolean
 isInfinite n = 
     n == Global.infinity || n == (-Global.infinity)
 
 
-{-| Turn any kind of value into a string. When you view the resulting string
-with `Text.fromString` it should look just like the value it came from.
-
-    toString 42 == "42"
-    toString [1,2] == "[1,2]"
-    toString "he said, \"hi\"" == "\"he said, \\\"hi\\\"\""
--}
+-- | Turn any kind of value into a string. When you view the resulting string
+-- | with `Text.fromString` it should look just like the value it came from.
+-- | 
+-- |     toString 42 == "42"
+-- |     toString [1,2] == "[1,2]"
+-- |     toString "he said, \"hi\"" == "\"he said, \\\"hi\\\"\""
+-- |
+-- | Equivalent to Purescript's `show`.
 toString :: forall a. (Show a) => a -> String
 toString = show
 
@@ -255,90 +274,96 @@ toString = show
 
 infixr 9 <<
 
-{-| Function composition, passing results along in the suggested direction. For
-example, the following code checks if the square root of a number is odd:
-
-    not << isEven << sqrt
-
-You can think of this operator as equivalent to the following:
-
-    (g << f)  ==  (\x -> g (f x))
-
-So our example expands out to something like this:
-
-    \n -> not (isEven (sqrt n))
--}
+-- | Function composition, passing results along in the suggested direction. For
+-- | example, the following code checks if the square root of a number is odd:
+-- | 
+-- |     not << isEven << sqrt
+-- | 
+-- | You can think of this operator as equivalent to the following:
+-- | 
+-- |     (g << f)  ==  (\x -> g (f x))
+-- | 
+-- | So our example expands out to something like this:
+-- | 
+-- |     \n -> not (isEven (sqrt n))
+-- |
+-- | Equivalent to Purescript's `<<<`.
 (<<) :: forall a b c. (b -> c) -> (a -> b) -> (a -> c)
 (<<) = (<<<)
 
 
 infixl 9 >>
 
-{-| Function composition, passing results along in the suggested direction. For
-example, the following code checks if the square root of a number is odd:
-
-    sqrt >> isEven >> not
-
-This direction of function composition seems less pleasant than `(<<)` which
-reads nicely in expressions like: `filter (not << isRegistered) students`
--}
+-- | Function composition, passing results along in the suggested direction. For
+-- | example, the following code checks if the square root of a number is odd:
+-- | 
+-- |     sqrt >> isEven >> not
+-- | 
+-- | This direction of function composition seems less pleasant than `(<<)` which
+-- | reads nicely in expressions like: `filter (not << isRegistered) students`
+-- |
+-- | Equivalent to Purescript's `>>>`.
 (>>) :: forall a b c. (a -> b) -> (b -> c) -> (a -> c)
 (>>) = (>>>)
 
 
 infixl 0 |>
 
-{-| Forward function application `x |> f == f x`. This function is useful
-for avoiding parenthesis and writing code in a more natural way.
-Consider the following code to create a pentagon:
-
-    scale 2 (move (10,10) (filled blue (ngon 5 30)))
-
-This can also be written as:
-
-    ngon 5 30
-      |> filled blue
-      |> move (10,10)
-      |> scale 2
--}
+-- | Forward function application `x |> f == f x`. This function is useful
+-- | for avoiding parenthesis and writing code in a more natural way.
+-- | Consider the following code to create a pentagon:
+-- | 
+-- |     scale 2 (move (10,10) (filled blue (ngon 5 30)))
+-- | 
+-- | This can also be written as:
+-- | 
+-- |     ngon 5 30
+-- |       |> filled blue
+-- |       |> move (10,10)
+-- |       |> scale 2
+-- |
+-- | Equivalent to Purescript's `#`.
 (|>) :: forall a b. a -> (a -> b) -> b
 (|>) = (#)
 
 
 infixr 0 <|
 
-{-| Backward function application `f <| x == f x`. This function is useful for
-avoiding parenthesis. Consider the following code to create a text element:
-
-    leftAligned (monospace (fromString "code"))
-
-This can also be written as:
-
-    leftAligned << monospace <| fromString "code"
--}
+-- | Backward function application `f <| x == f x`. This function is useful for
+-- | avoiding parenthesis. Consider the following code to create a text element:
+-- | 
+-- |     leftAligned (monospace (fromString "code"))
+-- | 
+-- | This can also be written as:
+-- | 
+-- |     leftAligned << monospace <| fromString "code"
+-- |
+-- | Equivalent to Purescript's `$`.
 (<|) :: forall a b. (a -> b) -> a -> b
 (<|) = ($)
 
 
-{-| Given a value, returns exactly the same value. This is called
-[the identity function](http://en.wikipedia.org/wiki/Identity_function).
--}
+-- | Given a value, returns exactly the same value. This is called
+-- | [the identity function](http://en.wikipedia.org/wiki/Identity_function).
+-- |
+-- | The Purescript equivalent is `id`.
 identity :: forall a. a -> a
 identity = id 
 
 
-{-| Create a [constant function](http://en.wikipedia.org/wiki/Constant_function),
-a function that *always* returns the same value regardless of what input you give.
-It is defined as:
-
-    always a b = a
-
-It totally ignores the second argument, so `always 42` is a function that always
-returns 42. When you are dealing with higher-order functions, this comes in
-handy more often than you might expect. For example, creating a zeroed out list
-of length ten would be:
-
-    map (always 0) [0..9]
--}
+-- | Create a [constant function](http://en.wikipedia.org/wiki/Constant_function),
+-- | a function that *always* returns the same value regardless of what input you give.
+-- | It is defined as:
+-- | 
+-- |     always a b = a
+-- | 
+-- | It totally ignores the second argument, so `always 42` is a function that always
+-- | returns 42. When you are dealing with higher-order functions, this comes in
+-- | handy more often than you might expect. For example, creating a zeroed out list
+-- | of length ten would be:
+-- | 
+-- |     map (always 0) [0..9]
+-- |
+-- | The Purescript equivalent is `const`.
 always :: forall a b. a -> b -> a
 always = const
