072986a807347c3422649caf2622fb99da8e1dab
Ryan Rempel <rgrempel@gmail.com>
2016-04-22 17:29:58 -0500

    More Elm.Graphics.Collage examples.

--- examples/Graphics/StaticCollage.purs
+++ examples/Graphics/StaticCollage.purs
@@ -9,13 +9,14 @@ module Examples.Graphics.StaticCollage where
 
 import Elm.Graphics.Collage
 import Elm.Graphics.Internal (setStyle)
-import Elm.Color (red)
+import Elm.Color (red, linear, radial, rgb, rgba, white)
 
 import Control.Monad.Eff (Eff)
 import Graphics.Canvas (Canvas)
 import Data.Nullable (toMaybe)
 import Data.Foldable (for_)
 import Data.List (List(..), (:))
+import Data.Tuple (Tuple(..))
 
 import DOM (DOM)
 import DOM.Renderable (class Renderable, render, renderIntoDOM, Position(AfterLastChild), defaultUpdate)
@@ -27,7 +28,7 @@ import DOM.Node.Types (elementToNode, ElementId(..), textToNode, documentToNonEl
 import DOM.Node.Document (createElement, createTextNode)
 import DOM.Node.Node (appendChild)
 
-import Prelude (bind, pure, Unit, (>>=), ($), (<$>))
+import Prelude (bind, pure, Unit, (>>=), ($), (<$>), negate)
 
 
 main :: ∀ e. Eff (canvas :: Canvas, dom :: DOM | e) Unit
@@ -90,7 +91,8 @@ instance renderableExample :: Renderable Example where
 
 examples :: List Example
 examples =
-    ( example1 : example2 : example3 : example4 : example5
+    ( example1 : example2 : example3 : example4 : example5 : example6
+    : example7 : example8
     : Nil
     )
 
@@ -183,3 +185,106 @@ example5 =
                 : Nil
                 )
         }
+
+
+-- TODO: This one doesn't seem to work
+example6 :: Example
+example6 =
+    Example
+        { caption:
+            """
+            makeCollage 100 100
+                ( textured "texture.jpg" (rect 100.0 100.0)
+                : Nil
+                )
+            """
+        , collage:
+            makeCollage 100 100
+                ( textured "texture.jpg" (rect 100.0 100.0)
+                : Nil
+                )
+        }
+
+
+-- TODO: This one is kind of upside-down compared to what it should be ...
+-- compare http://elm-lang.org/examples/linear-gradient
+example7 :: Example
+example7 =
+    Example
+        { caption: "Linear gradient"
+        , collage:
+            makeCollage 120 120
+                ( gradient grad (rect 120.0 120.0)
+                : Nil
+                )
+        }
+
+    where
+        grad =
+            linear
+                {x: 0.0, y: 60.0}
+                {x: 0.0, y: -60.0}
+                ( Tuple 0.0 (rgb 0 171 235)
+                : Tuple 0.79 white
+                : Tuple 0.8 (rgb 38 192 0)
+                : Tuple 1.0 white
+                : Nil
+                )
+
+
+-- TODO: This one is also close, but not quite right ...
+-- compare http://elm-lang.org/examples/radial-gradient
+example8 :: Example
+example8 =
+    Example
+        { caption: "Radial gradient"
+        , collage:
+            makeCollage 300 300
+                ( move {x: -55.0, y: -55.0} (gradient grad1 (circle 100.0))
+                : move {x:  40.0, y:  85.0} (gradient grad2 (circle 100.0))
+                : move {x:  50.0, y: -10.0} (gradient grad3 (circle 100.0))
+                : move {x: -10.0, y:  50.0} (gradient grad4 (circle 100.0))
+                : Nil
+                )
+        }
+
+    where
+        grad1 =
+            radial 
+                {x: 0.0, y:  0.0} 50.0
+                {x: 0.0, y: 10.0} 90.0
+                ( Tuple 0.0 (rgb  244 242 1)
+                : Tuple 0.8 (rgb  228 199 0)
+                : Tuple 1.0 (rgba 228 199 0 0.0)
+                : Nil
+                )
+
+        grad2 =
+            radial
+                {x: 0.0, y:  0.0} 15.0
+                {x: 7.0, y: -5.0} 40.0
+                ( Tuple 0.0 (rgb  0 201 255)
+                : Tuple 0.8 (rgb  0 181 226)
+                : Tuple 1.0 (rgba 0 181 226 0.0)
+                : Nil
+                )
+
+        grad3 =
+            radial
+                {x: 0.0, y:   0.0} 20.0
+                {x: 7.0, y: -15.0} 50.0
+                ( Tuple  0.0 (rgb  255 95 152)
+                : Tuple 0.75 (rgb  255 1 136)
+                : Tuple  1.0 (rgba 255 1 136 0.0)
+                : Nil
+                )
+
+        grad4 =
+            radial
+                {x: 0.0, y:  0.0} 10.0
+                {x: 7.0, y: -5.0} 30.0
+                ( Tuple 0.0 (rgb  167 211 12)
+                : Tuple 0.9 (rgb  1 159 98)
+                : Tuple 1.0 (rgba 1 159 98 0.0)
+                : Nil
+                )
