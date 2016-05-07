ba58059f55dd8dae6499c7f65e75257ff92346ed
Ryan Rempel <rgrempel@gmail.com>
2016-05-07 12:44:17 -0500

    Update docs for DOM.Renderable.

--- src/DOM/Renderable.purs
+++ src/DOM/Renderable.purs
@@ -1,34 +1,31 @@
 
--- | A module which ties together some general concepts for rendering things to the
--- | DOM.
--- |
--- | The general idea is that there are multiple approaches to taking some data
--- | structure rendering something to the DOM as a result.  One common element is
--- | the desire to be able to "update" the DOM based on changes (to avoid entirely
--- | replacing the DOM every time).  This module provides a general interface for
+-- | This module is intened to co-ordinate multiple approaches to taking a data
+-- | structure which represents a virtual DOM, and rendering it to the real DOM.
+-- | One common element is the desire to be able to "update" the DOM based on
+-- | differences between the previous data and the new data (to avoid entirely
+-- | replacing the DOM every time). This module provides a high-level interface for
 -- | that sort of thing.
 -- |
 -- | The hope is that this enables a degree of mix-and-match between different
--- | approaches. If each uses `DOM.Renderable`, and provides an instance for
--- | `DOM.Renderable`, then you imagine using them together in a nice way, even if
--- | they don't know about each other.  In any event, that's the hope.
--- |
--- | So, this module doesn't provide very much functionality as such. It is mostly
--- | meant as a possible rendezvous for other modules.
+-- | approaches to a virtual DOM. If each uses `DOM.Renderable`, and provides an
+-- | instance for `DOM.Renderable`, then you imagine using them together in a nice
+-- | way, even if they don't know about each other.
 -- |
 -- | ## Making something `Renderable`
 -- |
--- | The easy way to make something `Renderable` is to make it an instance
--- | of the `Renderable` class. So, you just implement the `render` and `update`
--- | methods ... see the discussion below for details.
+-- | To make something `Renderable`, make it an instance of the `Renderable` class.
+-- | So, you just implement the `render` function (and possibly the `update`
+-- | function -- see below for details). Note that the instance is only responsible
+-- | for returning the new (or updated) DOM -- it is not responsible for stitching
+-- | it into the real DOM.
 -- |
 -- | If you can implement the `render` and `update` methods, but it's awkward to
--- | create an instance, then you can also use the `makeDynamic` function to
--- | make a `DynamicRenderable`.
+-- | create an instance, then you can use the `makeDynamic` function to create a
+-- | `DynamicRenderable`.
 -- |
--- | Note that the best architecture is typically not to go straight from your
+-- | Note that the best architecture is often not to go straight from your
 -- | own specialized data types to `Renderable`, but instead to have a kind of
--- | virtual-DOM layer in between -- it is the virtual DOM types that would be
+-- | virtual-DOM layer in between. It is the virtual DOM types that would be
 -- | `Renderable`, rather than your own more specialized types. So, you would
 -- | typically have a set of functions to convert from your specialized types
 -- | to a virtual-DOM layer, and it is those virtual DOM types which would have
@@ -36,32 +33,57 @@
 -- |
 -- | ## Using a `Renderable`
 -- |
+-- | To use a `Renderable`, you can either:
+-- |
+-- | * use the instance functions (`render` or `update`) directly
+-- | * use `renderIntoDOM` or `updateDOM` to actually render or update the DOM
+-- | * use `renderOrUpdate` to automatically remember the previous data
+-- |
+-- | ### Using the instance directly.
+-- |
 -- | To use a `Renderable`, you can, of course, just use the `render` or
--- | `update` functions.
+-- | `update` functions directly.
 -- |
--- | However, neither of those methods necessarily modify the DOM, so you'll
--- | often want `renderIntoDOM` or `updateDOM` instead. (The `update` method
--- | may simply modify the node you pass in, but it may instead return a new node ...
--- | so you need to call `updateDOM` if you want to be sure to, well, update
--- | the DOM).
+-- | However, neither of those methods necessarily modify the DOM. The `render`
+-- | method only returns the new DOM, not yet inserted into the real DOM. The
+-- | `update` method may modify the node you pass in. but it may instead return a
+-- | new node.
 -- |
--- | ## Making it more dynamic
+-- | So, you may prefer to use the helper methods instead.
 -- |
+-- | ### Using the helpers
+-- |
+-- | You can use `renderIntoDOM` or `updateDOM` to actually modify the DOM.
+-- |
+-- | The `renderIntoDOM` function is fairly straightforward -- it uses the
+-- | `render` function to produce the new DOM, and then inserts it into the
+-- | real DOM in the specified manner.
+-- |
+-- | To use `updateDOM`, you'll need to remember the previous `Renderable` data somehow.
 -- | Now, a `Renderable` instance is parameterized by the type of the original
 -- | data. This is a shame, in a way, since you might want to do things like keep
 -- | a list of `Renderable` things -- that is, a list of things that are all
 -- | `Renderable`, but of different types. Furthermore, you might, in traversing
--- | a virtual DOM structure, want to use `update` in a situation where the previous
+-- | a virtual DOM structure, want to use `updateDOM` in a situation where the previous
 -- | `Renderable` may or may not have had the same underlying type.
 -- |
--- | To facilitate this sort of thing, we also have a `DynamicRenderable` type which,
--- | in effect, "erases" the type of the underlying data. This leaves you with a
--- | "simple" type that you can use in lists etc. Even better, the `DynamicRenderable`
--- | is itself an instance of `Renderable` -- isn't that nice? So you can use the usual
--- | functions, even if the underlying data type doesn't match.
+-- | To facilitate this sort of thing, we also have a `DynamicRenderable` type
+-- | which, in effect, "erases" the type of the underlying `Renderable` data. This
+-- | leaves you with a "simple" `DynamicRenderable` type that you can use in lists
+-- | etc. Even better, the `DynamicRenderable` is itself an instance of
+-- | `Renderable`. So you can use the usual functions, even if the underlying data
+-- | type doesn't match.
 -- |
 -- | To get a `DynamicRenderable` from a `Renderable`, take a look at `toDynamic`.
 -- | Alternative, you can also use `makeDynamic`, if it's awkward to make an instance.
+-- |
+-- | ### Automatically remembering the previous data
+-- |
+-- | For the ultimate in convenience, there is the `renderOrUpdate` method. It stores
+-- | the provided data in the DOM itself. Thus, you don't need to do anything to
+-- | remember the previous data -- `renderOrUpdate` will use it if it finds it where
+-- | it put it.
+
 
 module DOM.Renderable
     ( class Renderable, render, update, defaultUpdate
@@ -101,6 +123,12 @@ import Graphics.Canvas (Canvas)
 -- |   situation where no optimization is possible.  It simply calls `render`,
 -- |   ignoring the previous data and node.
 -- |
+-- |   Note that actually using `defaultUpdate` doesn't seem to be working, for
+-- |   some reason. However, you can manually write out a trivial update function
+-- |   in this way:
+-- |
+-- |       update rendered = render
+-- |
 -- | * So, calling `render` and `update` should produce the same results,
 -- |   so long as the `Rendered` parameter provided to `update` follows the rules.
 -- |
@@ -113,11 +141,19 @@ class Renderable a where
     update :: ∀ e. Rendered a -> a -> Eff (canvas :: Canvas, dom :: DOM | e) Node
 
 -- Originally, I didn't have `Canvas` in the row of effects, but it looks as though
--- you need to list anything that an instance might want to use ...
+-- you need to list anything that an instance might want to use. In a way,
+-- perhaps it is nice -- to be able to guarantee that an instance can't do any
+-- effects other than DOM or Canvas.
 
 
 -- | A default implementation of `update`, which just calls `render` on the
 -- | new data, ignoring the previous state.
+-- |
+-- | Note that the compiler doesn't seem to be happy if you use this in implementing
+-- | an instance. However, you can manually write out a trivial `update` implementation
+-- | as follows:
+-- |
+-- |       update rendered = render
 defaultUpdate :: ∀ a e. (Renderable a) => Rendered a -> a -> Eff (canvas :: Canvas, dom :: DOM | e) Node
 defaultUpdate rendered = render
 
@@ -155,7 +191,17 @@ instance renderableRenderableValue :: Renderable (RenderableValue a) where
             new.value
 
 
--- Makes a RenderableValue dynamic by stripping the underlying type
+-- | Makes a `Renderable` dynamic by stripping the underlying type.
+-- |
+-- | You can create a `DynamicRenderable` via `toDynamic` or `makeDynamic`.
+-- |
+-- | It's useful if you want to keep values in a `List` (so they need to
+-- | to have the same type), or if you are remembering previous values
+-- | for use via `update`, and they may or may not have the same underlying
+-- | type.
+-- |
+-- | There is a `Renderable` instance for `DynamicRenderable`, so you can
+-- | use it with the regular functions.
 newtype DynamicRenderable = DynamicRenderable (Exists RenderableValue)
 
 
@@ -224,6 +270,17 @@ data Position
     | ReplacingItself
 
 
+-- | Calls `render` on the provided value, and inserts the result into the DOM,
+-- | at the given position, relative to the provided node.
+-- |
+-- | This is unlike `render`, in that `render` merely produces the new DOM,
+-- | without inserting it anywhere.
+-- |
+-- | Once you've used `renderIntoDOM`, you could save the resulting `Rendered`
+-- | structure, and supply it to `updateDOM` when your data changes.
+-- |
+-- | Or, for even more convenience, you could consider `renderOrUpdate`, which
+-- | keeps track of the previous data for you.
 renderIntoDOM :: ∀ e a. (Renderable a) => Position -> Node -> a -> Eff (canvas :: Canvas, dom :: DOM | e) (Rendered a)
 renderIntoDOM position node value = do
     result <- render value
@@ -279,7 +336,9 @@ replaceNode old new = do
             appendChild new old
 
 
--- | Updates previously rendered data, replacing the originally rendered node in the DOM.
+-- | Updates previously data previously rendered via `renderToDOM`, replacing
+-- | the originally rendered node in the DOM. Returns a structure that can be
+-- | provided to `updateDOM` for future changes in the value.
 updateDOM :: ∀ e a. (Renderable a) => Rendered a -> a -> Eff (canvas :: Canvas, dom :: DOM | e) (Rendered a)
 updateDOM rendered value = do
     result <- update rendered value
@@ -299,9 +358,9 @@ foreign import getRenderable :: ∀ e. Element -> Eff (dom :: DOM | e) (Nullable
 -- | Renders the provided `Renderable` and inserts the result as a child of the
 -- | provided `Element`, replacing all other chidlren.
 -- |
--- } This stores the provided `Renderable` on the `Element`. So, when you call this again
--- | with the same `Element`, we can use the renderable's `update` method if it is the same type
--- | of renderable.
+-- | This stores the provided `Renderable` on the `Element`. So, when you call this again
+-- | with the same `Element`, we may be able to use the renderable's `update`
+-- | method, to efficiently update the DOM.
 renderOrUpdate :: ∀ e a. (Renderable a) => Element -> a -> Eff (canvas :: Canvas, dom :: DOM | e) Unit
 renderOrUpdate element renderable = do
     let
@@ -315,7 +374,9 @@ renderOrUpdate element renderable = do
         toMaybe <$> firstChild (elementToNode element)
 
     case {old, node} of
-        {old: Just value, node: Just result} ->
+        { old: Just value
+        , node: Just result
+        } ->
             -- We've got both an old value and a child, so try an update
             updateDOM {value, result} new
 
