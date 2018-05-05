e093c0ae56fa4d5da36ce4265b4bf6b255cf93eb
Ryan Rempel <rgrempel@gmail.com>
2018-05-05 11:46:56 -0500

    Quote the Elm documentation.

--- src/Elm/VirtualDom.purs
+++ src/Elm/VirtualDom.purs
@@ -172,21 +172,21 @@ newtype ThunkRecord3 msg a b c = ThunkRecord3
     }
 
 
--- | Create a DOM node with a tag name, a list of HTML properties that can
--- | include styles and event listeners, a list of CSS properties like `color`, and
--- | a list of child nodes.
--- |
--- |     import Json.Encode as Json
--- |
--- |     hello :: Node msg
--- |     hello =
--- |       node "div" [] [ text "Hello!" ]
--- |
--- |     greeting :: Node msg
--- |     greeting =
--- |       node "div"
--- |         [ property "id" (Json.string "greeting") ]
--- |         [ text "Hello!" ]
+-- | > Create a DOM node with a tag name, a list of HTML properties that can
+-- | > include styles and event listeners, a list of CSS properties like `color`, and
+-- | > a list of child nodes.
+-- | >
+-- | >     import Json.Encode as Json
+-- | >
+-- | >     hello :: Node msg
+-- | >     hello =
+-- | >       node "div" [] [ text "Hello!" ]
+-- | >
+-- | >     greeting :: Node msg
+-- | >     greeting =
+-- | >       node "div"
+-- | >         [ property "id" (Json.string "greeting") ]
+-- | >         [ text "Hello!" ]
 node :: ∀ msg. String -> List (Property msg) -> List (Node msg) -> Node msg
 node tag properties =
     PlainNode
@@ -200,10 +200,10 @@ node tag properties =
             organizeFacts properties
 
 
--- | Works just like `node`, but you add a unique identifier to each child
--- | node. You want this when you have a list of nodes that is changing: adding
--- | nodes, removing nodes, etc. In these cases, the unique identifiers help make
--- | the DOM modifications more efficient.
+-- | > Works just like `node`, but you add a unique identifier to each child
+-- | > node. You want this when you have a list of nodes that is changing: adding
+-- | > nodes, removing nodes, etc. In these cases, the unique identifiers help make
+-- | > the DOM modifications more efficient.
 keyedNode :: ∀ msg. String -> List (Property msg) -> List (Tuple String (Node msg)) -> Node msg
 keyedNode tag properties =
     KeyedNode
@@ -228,31 +228,31 @@ fromRenderable =
     Custom <<< toAnyRenderable
 
 
--- | Just put plain text in the DOM. It will escape the string so that it appears
--- | exactly as you specify.
--- |
--- |     text "Hello World!"
+-- | > Just put plain text in the DOM. It will escape the string so that it appears
+-- | > exactly as you specify.
+-- | >
+-- | >     text "Hello World!"
 text :: ∀ msg. String -> Node msg
 text = Text
 
 
 -- PROPERTIES
 
--- | When using HTML and JS, there are two ways to specify parts of a DOM node.
--- |
--- |   1. Attributes &mdash; You can set things in HTML itself. So the `class`
--- |      in `<div class="greeting"></div>` is called an *attribute*.
--- |
--- |   2. Properties &mdash; You can also set things in JS. So the `className`
--- |      in `div.className = 'greeting'` is called a *property*.
--- |
--- | So the `class` attribute corresponds to the `className` property. At first
--- | glance, perhaps this distinction is defensible, but it gets much crazier.
--- | *There is not always a one-to-one mapping between attributes and properties!*
--- | Yes, that is a true fact. Sometimes an attribute exists, but there is no
--- | corresponding property. Sometimes changing an attribute does not change the
--- | underlying property. For example, as of this writing, the `webkit-playsinline`
--- | attribute can be used in HTML, but there is no corresponding property!
+-- | > When using HTML and JS, there are two ways to specify parts of a DOM node.
+-- | >
+-- | >   1. Attributes &mdash; You can set things in HTML itself. So the `class`
+-- | >      in `<div class="greeting"></div>` is called an *attribute*.
+-- | >
+-- | >   2. Properties &mdash; You can also set things in JS. So the `className`
+-- | >      in `div.className = 'greeting'` is called a *property*.
+-- | >
+-- | > So the `class` attribute corresponds to the `className` property. At first
+-- | > glance, perhaps this distinction is defensible, but it gets much crazier.
+-- | > *There is not always a one-to-one mapping between attributes and properties!*
+-- | > Yes, that is a true fact. Sometimes an attribute exists, but there is no
+-- | > corresponding property. Sometimes changing an attribute does not change the
+-- | > underlying property. For example, as of this writing, the `webkit-playsinline`
+-- | > attribute can be used in HTML, but there is no corresponding property!
 data Property msg
     = CustomProperty Key Json.Value
     | Attribute Key String
@@ -264,7 +264,9 @@ data Property msg
 derive instance functorProperty :: Functor Property
 
 
--- | Transform the messages produced by a `Property`.
+-- | > Transform the messages produced by a `Property`.
+-- |
+-- | Equivalent to Purescript's `map`
 mapProperty :: ∀ a b. (a -> b) -> Property a -> Property b
 mapProperty = map
 
@@ -399,106 +401,106 @@ organizeFacts factList =
             }
 
 
--- | Create arbitrary *properties*.
--- |
--- |     import JavaScript.Encode as Json
--- |
--- |     greeting : Html
--- |     greeting =
--- |         node "div" [ property "className" (Json.string "greeting") ] [
--- |           text "Hello!"
--- |         ]
--- |
--- | Notice that you must give the *property* name, so we use `className` as it
--- | would be in JavaScript, not `class` as it would appear in HTML.
+-- | > Create arbitrary *properties*.
+-- | >
+-- | >     import JavaScript.Encode as Json
+-- | >
+-- | >     greeting : Html
+-- | >     greeting =
+-- | >         node "div" [ property "className" (Json.string "greeting") ] [
+-- | >           text "Hello!"
+-- | >         ]
+-- | >
+-- | > Notice that you must give the *property* name, so we use `className` as it
+-- | > would be in JavaScript, not `class` as it would appear in HTML.
 property :: ∀ msg. String -> Json.Value -> Property msg
 property = CustomProperty
 
 
--- | Create arbitrary HTML *attributes*. Maps onto JavaScript’s `setAttribute`
--- | function under the hood.
--- |
--- |     greeting : Html
--- |     greeting =
--- |         node "div" [ attribute "class" "greeting" ] [
--- |           text "Hello!"
--- |         ]
--- |
--- | Notice that you must give the *attribute* name, so we use `class` as it would
--- | be in HTML, not `className` as it would appear in JS.
+-- | > Create arbitrary HTML *attributes*. Maps onto JavaScript’s `setAttribute`
+-- | > function under the hood.
+-- | >
+-- | >     greeting : Html
+-- | >     greeting =
+-- | >         node "div" [ attribute "class" "greeting" ] [
+-- | >           text "Hello!"
+-- | >         ]
+-- | >
+-- | > Notice that you must give the *attribute* name, so we use `class` as it would
+-- | > be in HTML, not `className` as it would appear in JS.
 attribute :: ∀ msg. String -> String -> Property msg
 attribute = Attribute
 
 
--- | Would you believe that there is another way to do this?! This corresponds
--- | to JavaScript's `setAttributeNS` function under the hood. It is doing pretty
--- | much the same thing as `attribute` but you are able to have "namespaced"
--- | attributes. This is used in some SVG stuff at least.
+-- | > Would you believe that there is another way to do this?! This corresponds
+-- | > to JavaScript's `setAttributeNS` function under the hood. It is doing pretty
+-- | > much the same thing as `attribute` but you are able to have "namespaced"
+-- | > attributes. This is used in some SVG stuff at least.
 -- |
--- | * Note that the first argument is the namespace, the second the label, and
--- | third the value. *
+-- | Note that the first argument is the namespace, the second the label, and
+-- | third the value.
 attributeNS :: ∀ msg. String -> String -> String -> Property msg
 attributeNS = AttributeNS
 
 
--- | Specify a list of styles.
--- |
--- |     myStyle :: Property msg
--- |     myStyle =
--- |       style
--- |         [ Tuple "backgroundColor" "red"
--- |         , Tuple "height" "90px"
--- |         , Tuple "width" "100%"
--- |         ]
--- |
--- |     greeting :: Node msg
--- |     greeting =
--- |       node "div" [ myStyle ] [ text "Hello!" ]
+-- | > Specify a list of styles.
+-- | >
+-- | >     myStyle :: Property msg
+-- | >     myStyle =
+-- | >       style
+-- | >         [ Tuple "backgroundColor" "red"
+-- | >         , Tuple "height" "90px"
+-- | >         , Tuple "width" "100%"
+-- | >         ]
+-- | >
+-- | >     greeting :: Node msg
+-- | >     greeting =
+-- | >       node "div" [ myStyle ] [ text "Hello!" ]
 style :: ∀ msg. List (Tuple String String) -> Property msg
 style = Styles
 
 
 -- EVENTS
 
--- | Create a custom event listener.
--- |
--- |     import Json.Decode as Json
--- |
--- |     onClick : msg -> Property msg
--- |     onClick msg =
--- |       on "click" (Json.succeed msg)
--- |
--- | You first specify the name of the event in the same format as with JavaScript’s
--- | `addEventListener`. Next you give a JSON decoder, which lets you pull
--- | information out of the event object. If the decoder succeeds, it will produce
--- | a message and route it to your `update` function.
+-- | > Create a custom event listener.
+-- | >
+-- | >     import Json.Decode as Json
+-- | >
+-- | >     onClick : msg -> Property msg
+-- | >     onClick msg =
+-- | >       on "click" (Json.succeed msg)
+-- | >
+-- | > You first specify the name of the event in the same format as with JavaScript’s
+-- | > `addEventListener`. Next you give a JSON decoder, which lets you pull
+-- | > information out of the event object. If the decoder succeeds, it will produce
+-- | > a message and route it to your `update` function.
 on :: ∀ msg. String -> Json.Decoder msg -> Property msg
 on =
     (flip onWithOptions) defaultOptions
 
 
--- | Same as `on` but you can set a few options.
+-- | > Same as `on` but you can set a few options.
 onWithOptions :: ∀ msg. String -> Options -> Json.Decoder msg -> Property msg
 onWithOptions = OnEvent
 
 
--- | Options for an event listener. If `stopPropagation` is true, it means the
--- | event stops traveling through the DOM so it will not trigger any other event
--- | listeners. If `preventDefault` is true, any built-in browser behavior related
--- | to the event is prevented. For example, this is used with touch events when you
--- | want to treat them as gestures of your own, not as scrolls.
+-- | > Options for an event listener. If `stopPropagation` is true, it means the
+-- | > event stops traveling through the DOM so it will not trigger any other event
+-- | > listeners. If `preventDefault` is true, any built-in browser behavior related
+-- | > to the event is prevented. For example, this is used with touch events when you
+-- | > want to treat them as gestures of your own, not as scrolls.
 type Options =
     { stopPropagation :: Bool
     , preventDefault :: Bool
     }
 
 
--- | Everything is `False` by default.
--- |
--- |     defaultOptions =
--- |         { stopPropagation = False
--- |         , preventDefault = False
--- |         }
+-- | > Everything is `False` by default.
+-- | >
+-- | >     defaultOptions =
+-- | >         { stopPropagation = False
+-- | >         , preventDefault = False
+-- | >         }
 defaultOptions :: Options
 defaultOptions =
     { stopPropagation: false
@@ -508,36 +510,31 @@ defaultOptions =
 
 -- OPTIMIZATION
 
--- | A performance optimization that delays the building of virtual DOM nodes.
--- |
--- | Calling `(view model)` will definitely build some virtual DOM, perhaps a lot of
--- | it. Calling `(lazy view model)` delays the call until later. During diffing, we
--- | can check to see if `model` is referentially equal to the previous value used,
--- | and if so, we just stop. No need to build up the tree structure and diff it,
--- | we know if the input to `view` is the same, the output must be the same!
+-- | > A performance optimization that delays the building of virtual DOM nodes.
+-- | >
+-- | > Calling `(view model)` will definitely build some virtual DOM, perhaps a lot of
+-- | > it. Calling `(lazy view model)` delays the call until later. During diffing, we
+-- | > can check to see if `model` is referentially equal to the previous value used,
+-- | > and if so, we just stop. No need to build up the tree structure and diff it,
+-- | > we know if the input to `view` is the same, the output must be the same!
 lazy :: ∀ a msg. (Eq a) => (a -> Node msg) -> a -> Node msg
 lazy func arg =
     Thunk (mkExists (ThunkRecord1 {func, arg, lazy: defer \_ -> func arg}))
 
 
--- | Same as `lazy` but checks on two arguments.
+-- | > Same as `lazy` but checks on two arguments.
 lazy2 :: ∀ a b msg. Eq a => Eq b => (a -> b -> Node msg) -> a -> b -> Node msg
 lazy2 func arg1 arg2 =
     Thunk2 (mkExists2 (ThunkRecord2 {func, arg1, arg2, lazy: defer \_ -> func arg1 arg2}))
 
 
--- | Same as `lazy` but checks on three arguments.
+-- | > Same as `lazy` but checks on three arguments.
 lazy3 :: ∀ a b c msg. Eq a => Eq b => Eq c => (a -> b -> c -> Node msg) -> a -> b -> c -> Node msg
 lazy3 func arg1 arg2 arg3 =
     Thunk3 (mkExists3 (ThunkRecord3 {func, arg1, arg2, arg3, lazy: defer \_ -> func arg1 arg2 arg3}))
 
 
 {-
-
--- This one is going to be a bit tricky, because I've implemented Json.Decode in a
--- way that isn't going to be great for testing equality for Json.Decoder. I might
--- have to switch Json.Decode to more of a free monad approach. Which, in fact, would
--- roughly correspond to some changes in Json.Decode for Elm 0.17.
 
 function equalEvents(a, b)
 {
